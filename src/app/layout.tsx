b666f593419ab7a5793cc76ebf41ada228024e5e
sata rakotoson <rakotosonsata@gmail.com>
2025-08-13 09:50:58 +0300

    ajout de google analytcis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,6 +22,20 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={inter.className}>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-7TJPQXVLTT"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7TJPQXVLTT');
+          `}
+        </Script>
+        
         {children}
       </body>
     </html>
